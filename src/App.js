65a79265792c49f5cc8bccdad8ba044ad5780aec
yurika sakae <yurika.sun@gmail.com>
2019-08-05 14:23:37 -0700

    Form functionality complete WIP:toggle task completion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDom from "react-dom";
+
+import TodoForm from "./components/TodoComponents/TodoForm";
+import TodoList from "./components/TodoComponents/TodoList";
 
 const todoData = [
   {
@@ -18,10 +20,44 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todo: todoData
+    };
+  }
+
+  toggleItem = id => {
+    console.log(id);
+    this.setState({
+      todo: this.state.todo.map(item => {
+        if (item.id === id) {
+          return { ...item, completed: !item.completed };
+        } else {
+          return item;
+        }
+      })
+    });
+  };
+
+  addItem = itemName => {
+    const newItem = {
+      task: itemName,
+      id: Date.now,
+      completed: false
+    };
+    this.setState({
+      todo: [...this.state.todo, newItem]
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm addItem={this.addItem} />
+        <TodoList todo={this.state.todo} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,17 +4,28 @@ class TodoForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      item: ""
+      task: ""
     };
   }
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  submitItem = e => {
+    e.preventDefault();
+    this.props.addItem(this.state.task);
+  };
 
   render() {
     return (
       <form onSubmit={this.submitItem}>
         <input
           type="text"
-          value={this.item}
-          name="item"
+          value={this.task}
+          name="task"
           onChange={this.handleChanges}
         />
         <button>Add</button>
